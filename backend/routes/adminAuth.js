6479c8c4d575a1739011685e1e9c715ebae5b910
Pan <pangineering@gmail.com>
2025-07-05 08:08:05 +0700

    Backend APIs moved to a diffrent rep

--- backend/routes/adminAuth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { registerAdmin, loginAdmin } = require('../controllers/authControllerAdmin')
-
-router.post('/register', registerAdmin)
-router.post('/login', loginAdmin)
-
-module.exports = router
--- backend/routes/bookings.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../middleware/auth')
-const { createBooking, getBookings } = require('../controllers/bookingController')
-
-router.post('/', auth, createBooking)
-router.get('/', auth, getBookings)
-
-module.exports = router
